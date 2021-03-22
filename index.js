7a295c7712ccab7bf44eae1ed4746a379d01bd04
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-22 16:58:04 +0300

    downloading dataset

--- index.js
+++ index.js
@@ -41,29 +41,27 @@ const init = async () => {
 
   console.log('Alice token balance:', aliceBalance)
   console.log('Bob token balance:', bobBalance)
-try{
-    console.log("Try")
-    const dataService = await ocean.assets.createAccessServiceAttributes(
-        accounts[0],
-        10, // set the price in datatoken
-        new Date(Date.now()).toISOString().split(".")[0] + "Z", // publishedDate
-        0 // timeout
-      );
-    
-      // publish asset
-      const createData = await ocean.assets.create(
-        testData,
-        accounts[0],
-        [dataService],
-        tokenAddress
-      );
-    
-      const dataId = createData.id;
-      console.log('Data ID:', dataId);
-      console.log("Try Finished")
-} catch(error) {"error", error}
-  
 
+const dataService = await ocean.assets.createAccessServiceAttributes(
+    accounts[0],
+    10, // set the price in datatoken
+    new Date(Date.now()).toISOString().split(".")[0] + "Z", // publishedDate
+    0 // timeout
+    );
+
+    // publish asset
+    const createData = await ocean.assets.create(
+    testData,
+    accounts[0],
+    [dataService],
+    tokenAddress
+    );
+
+    const dataId = createData.id;
+    console.log('Data ID:', dataId);
+    
+    const dataDownload = await ocean.assets.simpleDownload(tokenAddress, urls.providerUri, transactionId, bob)
+    console.log('dataDownload', dataDownload)
 
 };
 
